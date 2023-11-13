489a9ef1260ce798d488ad4b0af18daf401b9a7a
dearwind153 <dearwind153@126.com>
2023-11-13 19:31:21 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 // main.cpp
 
 #include <iostream>
-#include <cstdint>
 
 extern "C" int add(int a, int b);
+extern "C" int sub(int a, int b);
 
 int main() {
     int a = 10;
     int b = 20;
     int c = add(a, b);
-    std::cout << "Result: " << c << std::endl;
+    int d = sub(a, b);
+    std::cout << "add("<<a<<","<<b<< ")Result: " << c << std::endl;
+    std::cout << "sub("<<a<<","<<b<< ")Result: " << d << std::endl;
     return 0;
 }
